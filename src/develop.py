bba597c4307c4c106a3c62d455e90a0d80e26317
Sander Kammeraat <kammeraat@145.118.EARLY-REGISTRATION.of.SURFnet.invalid>
2025-04-15 11:56:23 +0200

    gitignore

--- src/develop.py
+++ src/develop.py
@@ -24,6 +24,7 @@ data = np.loadtxt(test_trajectories_file_path,delimiter=",", skiprows=1)[:,1:]
 
 colnames =  np.loadtxt(test_trajectories_file_path, dtype=str,delimiter=",", max_rows=1)[1:]
 
+print(colnames)
 
 # column (name) to index
 c = dict()
@@ -32,16 +33,72 @@ for i, colname in enumerate(colnames):
     c[str(colname)] = i
 #%%
 
+#Now we can slice out the data like so:
+particle_numbers = data[:,c["particle"]]
 
-data[:,c["particle"]]
+frame_numbers = data[:, c["frame"]]
+
+#%%
+
+# To make analysis fast in Python we need to use vectorized operations.
+# This in turn means that we need to store the data as a numpy array.
+# Since numpy arrays have fixed sized, but we have variable numbers of particles,
+# we can use masked arrays:  https://numpy.org/doc/stable/reference/maskedarray.html 
+# To establish these, we have to find the maximum dimensions.
+
+max_frame_number = int(np.max(frame_numbers))
+min_frame_number = int(np.min(frame_numbers))
 
 
+max_particle_number = int(np.max(particle_numbers))
+min_particle_number = int(np.min(particle_numbers))
+
+Nparticles_max = max_particle_number - min_particle_number + 1
+
+Nframes = max_frame_number - min_frame_number + 1
+
+#%%
+
+
+# Set all values to be masked (True). Upon assignment, they will be unmasked.
+x = np.ma.masked_array( np.zeros( shape = (Nframes,Nparticles_max)), True )
+
+y = np.ma.masked_array( np.zeros( shape = (Nframes,Nparticles_max)), True )
+
+
+#%%
+
+# Let's now fill the arrays, by looping over time.
+
+for i  in range(Nframes):
+
+    #boolean to slice out data that belongs to frame i
+    frame_bools = data[:,c["frame"]]==i
+    
+    frame_i_data = data[frame_bools,:]
+    
+    frame_i_particle_inds = frame_i_data[:,c["particle"]].astype(int)
+    
+    
+    
+    x[i, frame_i_particle_inds] = frame_i_data[:,c["x"]]
+    
+    y[i, frame_i_particle_inds] = frame_i_data[:,c["y"]]
+    #to correctly place the particle data
+    #particle_inds = 
+    
+
+
+
+#%%
 fig, ax = plt.subplots()
 
 
-# f = 0
-# x = data[]
-# plt.scatter()
+plt.plot(x[:,0], y[:,0])
+
+plt.show()
+
+
 
 
 
